70a5ce93564a8f47061ccd58504e877dfea8f798
Reddy Mani Simha <itsmanisimha@gmail.com>
2025-10-05 20:34:06 +0530

    Add files via upload
    End term Project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+        colors :{
+            " color-mani":"#FF0000"
+        }
+    },
+  },
+  plugins: [],
+}
